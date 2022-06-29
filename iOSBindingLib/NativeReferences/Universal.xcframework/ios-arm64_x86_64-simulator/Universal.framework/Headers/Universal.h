e12fbc8890f999e8f7c1392605272a32177733e0
Adrian <adydecki@gmail.com>
2022-06-29 13:40:57 +0200

    Initial .net6-ios binding project to demonstrate compile time error

---
+++ iOSBindingLib/NativeReferences/Universal.xcframework/ios-arm64_x86_64-simulator/Universal.framework/Headers/Universal.h
@@ -0,0 +1,16 @@
+#import <Foundation/Foundation.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+int theUltimateAnswer (void);
+
+@interface FrameworkTest : NSObject {
+}
+    -(int) func;
+@end
+
+#ifdef __cplusplus
+} /* extern "C" */
+#endif
